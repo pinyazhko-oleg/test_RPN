5fe8ae7afd66a385b4ca5a5316a108c4318efaf5
pinyazhko-oleg <pinyazchko.oleg@gmail.com>
2018-01-19 17:11:38 +0200

    calc

--- test_rpn.js
+++ test_rpn.js
@@ -15,3 +15,40 @@ https.get('https://www.eliftech.com/school-task', (resp) => {
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
+
+let array = JSON.parse(data).expressions;
+
+let calc = function MathSolver() {
+
+    this.solvePostfix = function(postfix) {
+        var resultStack = [];
+        postfix = postfix.split(" ");
+        for(var i = 0; i < postfix.length; i++) {
+            if(postfix[i].isNumeric()) {
+                resultStack.push(postfix[i]);
+            } else {
+                var a = resultStack.pop();
+                var b = resultStack.pop();
+                if(postfix[i] === "+") {
+                    resultStack.push(parseInt(a) + parseInt(b));
+                } else if(postfix[i] === "-") {
+                    resultStack.push(parseInt(b) - parseInt(a));
+                } else if(postfix[i] === "*") {
+                    resultStack.push(parseInt(a) * parseInt(b));
+                } else if(postfix[i] === "/") {
+                    resultStack.push(parseInt(b) / parseInt(a));
+                } else if(postfix[i] === "^") {
+                    resultStack.push(Math.pow(parseInt(b), parseInt(a)));
+                }
+            }
+        }
+        if(resultStack.length > 1) {
+            return "error";
+        } else {
+            return resultStack.pop();
+        }
+    }
+
+};
+
+
